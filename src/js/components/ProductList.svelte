351b3ea137f6b58398f45b3e8fdc26355443ac3f
nortories <sooaemalelagij@gmail.com>
2024-02-02 13:57:09 -0700

    Team activity 3
    Running into errors on step 5 of team activity 3

---
+++ src/js/components/ProductList.svelte
@@ -0,0 +1,11 @@
+<script type="module">
+    import { getData } from "../productData.mjs";
+    
+    // this is how we make a prop in svelte
+    export let category;
+    // if you are looking at this thinking that's strange to just stop with a promise
+    // you would be right.  This will make more sense in a bit...stay tuned.
+    let promise = getData(category);
+    </script>
+    
+    <p>Top products: {category}</p>
